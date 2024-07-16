66e29709c3b4c39408892663d40dbf8f65f789c4
Denis Davydov <dane.tno@gmail.com>
2024-07-16 22:30:17 +0300

    first commit

---
+++ src/types/index.ts
@@ -0,0 +1,33 @@
+export interface IProduct {
+  id: string;
+  image: string;
+  category: string;
+  title: string;
+  description: string;
+  price: number | null;
+}
+
+export interface IFormOrder {
+  items: string[];
+  payment: string;
+  address: string;
+  email: string;
+  phone: string;
+  total: number;
+}
+
+export interface IProductData {
+  catalog: IProduct[];
+  preview: string | null;
+  order: IFormOrder;
+}
+
+export interface IFormOrderResult {
+  id: string;
+  total: number;
+}
+
+export type TBasketProduct = Pick<IProduct, 'id' | 'title' | 'description'>;
+
+export type TOrderInfo = Pick<IFormOrder, 'payment' | 'address' | 'email' | 'phone'>;
+
